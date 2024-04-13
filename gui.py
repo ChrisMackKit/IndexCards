3791dab1f57045eb20925e9f3c57006fdc7f9c5e
ChrisMackKit <mack_christian@t-online.de>
2024-04-13 02:04:21 +0200

    started connecting GUI with JSON functionality and GUI nearly done

--- gui.py
+++ gui.py
@@ -1,96 +1,174 @@
 import tkinter as tk
+import jsonEdit as je
+
+
+# Class for everything related to the interface
 
 class GUI:
+        #There will be one list with all the topics and a corresponding
 
-    def __init__(self) -> None:
+        def __init__(self) -> None:
         
-        self.question = "Was ist die Hauptstadt von Deutschland?"
-        self.answer = "Berlin"
-        self.box = 1
-        self.stats = [True, False, False, True]
-        self.topics = ['Hauptstädte', 'Flüsse', 'Informatik']
+                self.jsonData = je.openJSON()
+                
+                self.cardsList = []
+                
+                self.question = "Choose a topic you want to learn"
+                self.answer = " "
+
+                self.box = 1
+                self.stats = []
+                self.topics = ['capitals', 'rivers']
+                
+                self.cardIndex = 0
+
+                self.window = tk.Tk()
+                self.window.geometry("800x400")
+                self.window.title("Index Card Program")
+
+                # Texts and labels
+                self.questionLabel = tk.Label(self.window, text = self.question, font=('Arial', 18))
+                self.questionLabel.pack(padx= 0, pady=20)
+
+                self.answerText = tk.Text(self.window, height = 1)
+                self.answerText.pack(padx = 0, pady = 10)
+
+                self.answerLabel = tk.Label(self.window, text = "")
+                self.answerLabel.pack(padx = 0, pady = 10)
+
+                self.statsBox = tk.Label(self.window, text = self.stats, font=(14))
+                self.statsBox.place(x = 300, y=240)
+                
+                # Button of the GUI
+                self.showAnswer = tk.Button(self.window, text = "show answer", command = self.showAnswer, font=('Arial', 18))
+                self.showAnswer.place(x= 90, y=120, height=30, width=200)
+                
+                self.correctAnswer = tk.Button(self.window, text = "correct answer", command = self.correctAnswer, font=('Arial', 18))
+                self.correctAnswer.place(x= 300, y=120, height=30, width=200)
+                
+                self.wrongAnswer = tk.Button(self.window, text = "wrong answer", command = self.wrongAnswer, font=('Arial', 18))
+                self.wrongAnswer.place(x= 510, y=120, height=30, width=200)
+                
+                self.addTopic = tk.Button(self.window, text = "add new topic", command = self.addTopic, font=('Arial', 18))
+                self.addTopic.place(x= 90, y= 150, height=30, width=200)
+                
+                self.addCard = tk.Button(self.window, text = "add new card", command = self.addCard, font=('Arial', 18))
+                self.addCard.place(x= 300, y= 150, height=30, width=200)
+
+                # Drop down menu
+                self.clicked = tk.StringVar()
+                self.clicked.set('Choose Topic')
+                self.clicked.trace_add('write', self.callback)
+                self.topicMenu = tk.OptionMenu(self.window, self.clicked, *self.topics)
+                self.topicMenu.place(x=80, y= 300)
+
+                
+                self.window.mainloop()
+
+        def callback(self, *args):
+                # if topic is changed, the list of cards changes
+                listOfCards = je.getListOfCards(self.clicked.get(), 'cardsBox1')
+                print('Topic: ' + self.clicked.get())
+                print(listOfCards)
+                self.cardsList = listOfCards
+                self.questionLabel['text'] = listOfCards[0][0][0]
+                self.answer = listOfCards[0][1][0]
+                self.statsBox['text'] = listOfCards[0][2][0]
+                
+
+        def showAnswer(self):
+                #answerLabel.configure(text = answer)
+                #show the answer in the window
+                print("okay")
+                self.answerLabel['text'] = self.answer
+                self.answerLabel = tk.Label(self.window, text = self.answer, font=('Arial', 16))
+                self.answerLabel.place(x = 350, y = 200)
+
+        def correctAnswer(self):
+                #updates stats and goes to next question
+                print("correct")
+                if len(self.stats) > 4:
+                        self.stats.pop(0)
+                self.stats.append(True)
+                self.statsBox.destroy()
+                self.statsBox = tk.Label(self.window, text = self.stats, font=(14))
+                self.statsBox.place(x = 300, y=240)
+                self.nextQuestion()
+
+        def wrongAnswer(self):
+                #updates stats and goes to next question
+                print("wrong")
+                if len(self.stats) > 4:
+                        self.stats.pop(0)
+                self.stats.append(False)
+                self.statsBox.destroy()
+                self.statsBox = tk.Label(self.window, text = self.stats, font=(14))
+                self.statsBox.place(x = 300, y=240)
+                self.nextQuestion()
         
 
-        self.window = tk.Tk()
-        self.window.geometry("800x400")
-        self.window.title("Index Card Program")
+        def nextQuestion(self):
+                self.cardIndex += 1
+                if self.cardIndex >= len(self.cardsList):
+                        self.cardIndex = 0
+                self.questionLabel['text'] = self.cardsList[self.cardIndex][0][0]
+                self.answer = self.cardsList[self.cardIndex][1][0]
+                self.statsBox['text'] = self.cardsList[self.cardIndex][2][0]
+                self.answerLabel['text'] = ''
 
-        # Texts and labels
-        self.questionLabel = tk.Label(self.window, text = self.question, font=('Arial', 18))
-        self.questionLabel.pack(padx= 0, pady=20)
 
-        self.answerText = tk.Text(self.window, height = 1)
-        self.answerText.pack(padx = 0, pady = 10)
-
-        self.answerLabel = tk.Label(self.window, text = "")
-        self.answerLabel.pack(padx = 0, pady = 10)
-
-        self.statsBox = tk.Label(self.window, text = self.stats, font=(14))
-        self.statsBox.place(x = 300, y=240)
-        
-        # Button of the GUI
-        self.showAnswer = tk.Button(self.window, text = "show answer", command = self.showAnswer, font=('Arial', 18))
-        self.showAnswer.place(x= 90, y=120, height=30, width=200)
-        
-        self.correctAnswer = tk.Button(self.window, text = "correct answer", command = self.correctAnswer, font=('Arial', 18))
-        self.correctAnswer.place(x= 300, y=120, height=30, width=200)
-        
-        self.wrongAnswer = tk.Button(self.window, text = "wrong answer", command = self.wrongAnswer, font=('Arial', 18))
-        self.wrongAnswer.place(x= 510, y=120, height=30, width=200)
-        
-        self.addTopic = tk.Button(self.window, text = "add new topic", command = self.addTopic, font=('Arial', 18))
-        self.addTopic.place(x= 90, y= 150, height=30, width=200)
-
-        # Drop down menu
-        self.clicked = tk.StringVar()
-        self.clicked.set('Choose Topic')
-        self.topicMenu = tk.OptionMenu(self.window, self.clicked, *self.topics)
-        self.topicMenu.place(x=80, y= 300)
-
-        self.window.mainloop()
-
-    def showAnswer(self):
-            #answerLabel.configure(text = answer)
-            #show the answer in the window
-            print("okay")
-            self.answerLabel = tk.Label(self.window, text = self.answer, font=('Arial', 16)).place(x = 350, y = 200)
-
-    def correctAnswer(self):
-            #updates stats and goes to next question
-            print("correct")
-            self.stats.pop(0)
-            self.stats.append(True)
-            self.statsBox.destroy()
-            self.statsBox = tk.Label(self.window, text = self.stats, font=(14))
-            self.statsBox.place(x = 300, y=240)
-
-    def wrongAnswer(self):
-            #updates stats and goes to next question
-            print("wrong")
-            self.stats.pop(0)
-            self.stats.append(False)
-            self.statsBox.destroy()
-            self.statsBox = tk.Label(self.window, text = self.stats, font=(14))
-            self.statsBox.place(x = 300, y=240)
-
-    def addTopic(self):
-            #adds new topic to the JSON
-            print("new topic created")
-            self.addTopicWindow = tk.Toplevel(self.window)
-            self.addTopicWindow.title('Add New Topic')
-            self.addTopicWindow.geometry('500x150')
+        def addTopic(self):
+                #adds new topic to the JSON
+                print("new topic created")
+                self.addTopicWindow = tk.Toplevel(self.window)
+                self.addTopicWindow.title('Add New Topic')
+                self.addTopicWindow.geometry('500x150')
+                
+                
+                self.addTopicText = tk.Label(self.addTopicWindow, text ='Enter the name for the new topic: ')
+                self.addTopicText.pack()
+                self.addTopicButton = tk.Button(self.addTopicWindow, text = "add topic", command = self.addTopic2, font=('Arial', 18))
+                self.addTopicButton.place(x= 150, y=100, height=30, width=200)
+                self.topicText = tk.Text(self.addTopicWindow, height = 1)
+                self.topicText.pack(padx = 0, pady = 10)
             
-            
-            self.addTopicText = tk.Label(self.addTopicWindow, text ='Enter the name for the new topic: ')
-            self.addTopicText.pack()
-            self.addTopicButton = tk.Button(self.addTopicWindow, text = "add topic", command = self.addTopic2, font=('Arial', 18))
-            self.addTopicButton.place(x= 150, y=100, height=30, width=200)
-            self.topicText = tk.Text(self.addTopicWindow, height = 1)
-            self.topicText.pack(padx = 0, pady = 10)
-            
+        def addTopic2(self):
+                topicName = self.topicText.get('1.0', 'end -1c')
+                print('topic added: ' + topicName)
 
-    def addTopic2(self):
-        topicName = self.topicText.get('1.0', tk.END)
-        print('topic added: ' + topicName)
 
+        def addCard(self):
+                #adds new card to a topic
+                print("new card created")
+                self.addCardWindow = tk.Toplevel(self.window)
+                self.addCardWindow.title('Add New Card')
+                self.addCardWindow.geometry('500x180')
+                
+                
+                self.addCardLabel = tk.Label(self.addCardWindow, text ='Enter the question of the card: ')
+                self.addCardLabel.pack()
+                self.cardQuestion = tk.Text(self.addCardWindow, height = 1)
+                self.cardQuestion.pack(padx = 0, pady = 10)
+                self.addCardAnswerLabel = tk.Label(self.addCardWindow, text ='Enter the anwser of the card: ')
+                self.addCardAnswerLabel.pack()
+                self.cardAnswer = tk.Text(self.addCardWindow, height = 1)
+                self.cardAnswer.pack(padx = 0, pady = 10)
+                self.clicked2 = tk.StringVar()
+                self.clicked2.set('Choose Topic')
+                self.topicMenu2 = tk.OptionMenu(self.addCardWindow, self.clicked2, *self.topics)
+                self.topicMenu2.place(x=20, y= 130)
+                self.addTopicButton = tk.Button(self.addCardWindow, text = "add topic", command = self.addCard2, font=('Arial', 18))
+                self.addTopicButton.place(x= 150, y=130, height=30, width=200)
+            
+        def addCard2(self):
+                cardQuestion = self.cardQuestion.get('1.0', 'end -1c')
+                cardAnswer = self.cardAnswer.get('1.0', 'end -1c')
+                topicOfCard = self.clicked2.get()
+                if topicOfCard == 'Choose Topic':
+                        print('You have to pick a topic')
+                else:
+                        print('card added. Question: ' + cardQuestion + ' Answer: ' + cardAnswer + ' Topic: ' + topicOfCard)
+                        self.jsonData = je.addCardToTopic(topicOfCard, cardQuestion, cardAnswer, self.jsonData)
+                        je.saveJSON("cards.json", self.jsonData)
+        
 GUI()
--- jsonEdit.py
+++ jsonEdit.py
@@ -2,26 +2,26 @@ import json
 
 
 # 'cards.json'
-def openJSON(jsonFile):
+def openJSON(jsonFile='cards.json'):
     with open(jsonFile) as f:
         content = json.load(f)
     return content
 
 
 # jsonFile is the name of the file to overwrite or create, content is the json (dic) that is to be saved
-def saveJSON(jsonFile, content):
+def saveJSON(jsonFile='cards.json', content=openJSON("cards.json")):
     with open(jsonFile, 'w') as f:
         json.dump(content, f, indent=4)
 
 
-def showTopics(content):
+def showTopics(content = openJSON("cards.json")):
     topics = []
     for i in content["topics"]:
         topics.append(i["topicName"])
     return topics
 
 
-def showCards(content, topic, box):
+def showCards(topic, box = 'cardsBox1', content = openJSON("cards.json")):
     questions = []
     for i in content['topics']:
         if i["topicName"] == topic:
@@ -29,6 +29,14 @@ def showCards(content, topic, box):
                 questions.append(i['cardsBox1'][n]["question"])
     return questions
 
+# with a given topic and box it returns a list of all the cards and every card is a list with 3 entries ['Question', 'Answer', [stats]]
+def getListOfCards(topic, box = 'cardsBox1', content = openJSON("cards.json")):
+    listOfCards = []
+    for i in content['topics']:
+        if i['topicName'] == topic:
+            for n in range(len(i[box])):
+                listOfCards.append([[i['cardsBox1'][n]["question"]], [i['cardsBox1'][n]["answer"]], [i['cardsBox1'][n]["stats"]]])
+    return listOfCards
 
 def convertBox(box):
     match box:
@@ -44,15 +52,18 @@ def convertBox(box):
             return "cardsBox5"
 
 
-def addCardToTopic(content, topic, question, answer):
+def addCardToTopic(topic, question, answer, content = openJSON("cards.json")):
     newDic = {"question": question, "answer": answer, "stats": []}
     for i in content['topics']:
         if i["topicName"] == topic:
             i["cardsBox1"].append(newDic)
     return content
 
+#type: 'dict'
 content = openJSON("cards.json")
+
 # addCardToTopic(content, "capitals", "Was ist die Hauptstadt von Iran?", "Teheran")
 # print(content)
 # saveJSON("cards2.json", content)
-# print(showCards(content, "rivers", "cardsBox1"))
+#print(getListOfCards("rivers", "cardsBox1"))
+#saveJSON("cards.json", addCardToTopic('rivers', 'test frage', 'test antwort'))
